8c8209f813f1ef46d293c08a6c9e73faa303e0e8
EC2 Default User <ec2-user@ip-172-31-36-190.us-east-2.compute.internal>
2020-03-04 12:14:38 +0000

    second

--- resources/views/download/top.blade.php
+++ resources/views/download/top.blade.php
@@ -7,25 +7,15 @@
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <br>
-                <br>
-                <br>
-                <img src="{{ asset('/images/giftchat.png') }}"></img>
-                <br>
-                <br>
-                <P>ただいま公式アプリダウンロードページへ移動中です。<br>下記より対象OSを選択して下さい。</P>
-                <a href="https://preaf.jp/pa.do?s=v04031&o=45028&guid=ON"><img src="{{ asset('/images/ios.png') }}"></img></a>
-                <br>
-                <a href="https://preaf.jp/pa.do?s=v04031&o=45028&guid=ON"><img src="{{ asset('/images/android.png') }}"></img></a>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-            </div>
-        </div>
+    <div class="body">
+        <br />
+        <br />
+        <br />
+        <img src="{{ asset('/images/gf_2.png') }}"></img>
+        <br />
+        <br />
+        <br />
+        <p>ただいま公式アプリダウンロードページへ移動中です。<br>下記より対象OSを選択して下さい。</p>
+        <a href="https://preaf.jp/pa.do?s=v04031&o=45028&guid=ON"><img src="{{ asset('/images/d_2.png') }}"></img></a>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('download.top');
 });
 
 Route::get('download/top', 'DownloadController@top');
