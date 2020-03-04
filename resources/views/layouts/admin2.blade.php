43b7aa04c9ae97834ff2ae6b400c6ef6d8303bc9
EC2 Default User <ec2-user@ip-172-31-36-190.us-east-2.compute.internal>
2020-03-04 23:38:44 +0000

    commit

--- resources/views/layouts/admin2.blade.php
+++ resources/views/layouts/admin2.blade.php
@@ -1,34 +1,34 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="refresh" content=" 3; url=https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669">
+<!--<!DOCTYPE html>-->
+<!--<html lang="{{ app()->getLocale() }}">-->
+<!--    <head>-->
+<!--        <meta charset="utf-8">-->
+<!--        <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+<!--        <meta name="viewport" content="width=device-width, initial-scale=1">-->
+<!--        <meta http-equiv="refresh" content=" 3; url=https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669">-->
 
         <!-- CSRF Token -->
-         {{-- 後の章で説明します --}}
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!--         {{-- 後の章で説明します --}}-->
+<!--        <meta name="csrf-token" content="{{ csrf_token() }}">-->
 
-        {{-- 各ページごとにtitleタグを入れるために@yieldで空けておきます。 --}}
-        <title>@yield('title')</title>
+<!--        {{-- 各ページごとにtitleタグを入れるために@yieldで空けておきます。 --}}-->
+<!--        <title>@yield('title')</title>-->
 
         <!-- Scripts -->
-         {{-- Laravel標準で用意されているJavascriptを読み込みます --}}
+<!--         {{-- Laravel標準で用意されているJavascriptを読み込みます --}}-->
         <!--<script src="{{ secure_asset('js/app.js') }}" defer></script>-->
 
         <!-- Fonts -->
-        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+<!--        <link rel="dns-prefetch" href="https://fonts.gstatic.com">-->
+<!--        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">-->
 
         <!-- Styles -->
-        {{-- Laravel標準で用意されているCSSを読み込みます --}}
+<!--        {{-- Laravel標準で用意されているCSSを読み込みます --}}-->
         <!--<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">-->
-        {{-- この章の後半で作成するCSSを読み込みます --}}
-        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        <div id="app">
+<!--        {{-- この章の後半で作成するCSSを読み込みます --}}-->
+<!--        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">-->
+<!--    </head>-->
+<!--    <body>-->
+<!--        <div id="app">-->
             <!--{{-- 画面上部に表示するナビゲーションバーです。 --}}-->
             <!--<nav class="navbar navbar-expand-md navbar-dark navbar-laravel">-->
             <!--    <div class="container">-->
@@ -53,10 +53,10 @@
             <!--</nav>-->
             <!--{{-- ここまでナビゲーションバー --}}-->
 
-            <main class="py-4">
-                {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
-                @yield('content')
-            </main>
-        </div>
-    </body>
-</html>
+<!--            <main class="py-4">-->
+<!--                {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}-->
+<!--                @yield('content')-->
+<!--            </main>-->
+<!--        </div>-->
+<!--    </body>-->
+<!--</html>-->
