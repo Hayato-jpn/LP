6d5465bac60429e64d7bf96f7a24157225ace15b
EC2 Default User <ec2-user@ip-172-31-36-190.us-east-2.compute.internal>
2020-03-05 09:23:21 +0000

    add

--- resources/views/download/top2.blade.php
+++ resources/views/download/top2.blade.php
@@ -1,17 +1,17 @@
-<!--{{-- layouts/admin.blade.phpを読み込む --}}-->
-<!--@extends('layouts.admin2')-->
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin2')
 
 
-<!--{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}-->
-<!--@section('title', 'OSを選択して下さい')-->
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'OSを選択して下さい')
 
-<!--{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}-->
-<!--@section('content')-->
-<!--    <div class="body">-->
-<!--        <br />-->
-<!--        <br />-->
-<!--        <br />-->
-<!--        <p>ただいま公式アプリダウンロードページへ移動中です。<br>しばらくお待ち下さい。</p>-->
-<!--        <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669"><img src="{{ asset('/images/d_2.png') }}"></img></a>-->
-<!--    </div>-->
-<!--@endsection-->
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="body">
+        <br />
+        <br />
+        <br />
+        <p>ただいま公式アプリダウンロードページへ移動中です。<br>対象OSを選択して下さい。</p>
+        <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8126&site_id=27669"><img src="{{ asset('/images/d_2.png') }}"></img></a>
+    </div>
+@endsection
