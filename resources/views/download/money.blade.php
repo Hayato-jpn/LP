adecd72db8af6bfab2a2cc570f89a8b6d3f42836
EC2 Default User <ec2-user@ip-172-31-36-190.us-east-2.compute.internal>
2020-03-21 09:12:38 +0000

    img edit

--- resources/views/download/money.blade.php
+++ resources/views/download/money.blade.php
@@ -14,35 +14,35 @@
         <h3>学校やバイト、仕事の隙間時間に🎵</h3>
         <br />
         <br />
-        <img src="{{ asset('/images/attention.png') }}"></img>
+        <img class="img-width" src="{{ asset('/images/attention.png') }}"></img>
         <div class="line"></div>
         <!--Biscor-->
-        <img src="{{ asset('/images/biscor.png') }}"></img>
+        <img class="img-width" src="{{ asset('/images/biscor.png') }}"></img>
         <p>【初心者必見！？】ライブ配信型アプリ🎵</p>
         <a href="#" class="btn-square">公式ページへ</a>
         <div class="line"></div>
         <!--モア-->
-        <img src="{{ asset('/images/moa.png') }}"></img>
+        <img class="img-width" src="{{ asset('/images/moa.png') }}"></img>
         <p>LINE感覚でお話するだけ🎵<br />簡単お小遣いゲット！</p>
         <a href="#" class="btn-square">公式ページへ</a>
         <div class="line"></div>
         <!--ギフトチャット-->
-        <img src="{{ asset('/images/giftchat.png') }}"></img>
+        <img class="img-width" src="{{ asset('/images/giftchat.png') }}"></img>
         <p>Amazon、iTunesカードが貰える!!??<br />ポイントを貯めて無料でゲット！</p>
         <a href="#" class="btn-square">App Storeへ</a>
         <div class="line"></div>
         <!--ベリー-->
-        <img src="{{ asset('/images/berry.png') }}"></img>
+        <img class="img-width" src="{{ asset('/images/berry.png') }}"></img>
         <p>【髪型服装自由！】<br />自宅で出来るチャットレディのお仕事🎵</p>
         <a href="#" class="btn-square">公式ページへ</a>
         <div class="line"></div>
         <!--チャリン-->
-        <img src="{{ asset('/images/charin.png') }}"></img>
+        <img class="img-width" src="{{ asset('/images/charin.png') }}"></img>
         <p>【SNSで稼ぐ!!??】SNS型お小遣いアプリ🎵</p>
         <a href="#" class="btn-square">公式ページへ</a>
         <div class="line"></div>
         <!--ワクワク-->
-        <img src="{{ asset('/images/wakuwaku.png') }}"></img>
+        <img class="img-width" src="{{ asset('/images/wakuwaku.png') }}"></img>
         <p>【平均お手当時給 1万円超え!!??】<br />大手企業運営パパ活アプリ🎵</p>
         <a href="#" class="btn-square">紹介ページへ</a>
         <div class="line"></div>
