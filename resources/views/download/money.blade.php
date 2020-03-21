76be9bb2e6c7c287cdca503a36733cfab99978e4
EC2 Default User <ec2-user@ip-172-31-36-190.us-east-2.compute.internal>
2020-03-21 09:25:47 +0000

    target=_blank

--- resources/views/download/money.blade.php
+++ resources/views/download/money.blade.php
@@ -19,32 +19,32 @@
         <!--Biscor-->
         <img class="img-width" src="{{ asset('/images/biscor.png') }}"></img>
         <p>【初心者必見！？】<br /><span>ライブ配信型アプリ🎵</span></p>
-        <a href="https://a-trade.jp/redirect/biscor?media=G16531" class="btn-square">公式ページへ</a>
+        <a href="https://a-trade.jp/redirect/biscor?media=G16531" class="btn-square" target="_blank">公式ページへ</a>
         <div class="line"></div>
         <!--モア-->
         <img class="img-width" src="{{ asset('/images/moa.png') }}"></img>
         <p><span>LINE感覚でお話するだけ🎵</span><br />簡単お小遣いゲット！</p>
-        <a href="https://a-trade.jp/redirect/moa?media=G16531" class="btn-square">公式ページへ</a>
+        <a href="https://a-trade.jp/redirect/moa?media=G16531" class="btn-square" target="_blank">公式ページへ</a>
         <div class="line"></div>
         <!--ギフトチャット-->
         <img class="img-width" src="{{ asset('/images/giftchat.png') }}"></img>
         <p><span>Amazon、iTunesカードが貰える!!??</span><br />ポイントを貯めて無料でゲット！</p>
-        <a href="https://a-trade.jp/redirect/giftchat?media=G16531" class="btn-square">App Storeへ</a>
+        <a href="https://a-trade.jp/redirect/giftchat?media=G16531" class="btn-square" target="_blank">App Storeへ</a>
         <div class="line"></div>
         <!--ベリー-->
         <img class="img-width" src="{{ asset('/images/berry.png') }}"></img>
         <p><span>【髪型服装自由！】</span><br />自宅で出来るチャットレディのお仕事🎵</p>
-        <a href="https://a-trade.jp/redirect/berry?media=G16531" class="btn-square">公式ページへ</a>
+        <a href="https://a-trade.jp/redirect/berry?media=G16531" class="btn-square" target="_blank">公式ページへ</a>
         <div class="line"></div>
         <!--チャリン-->
         <img class="img-width" src="{{ asset('/images/charin.png') }}"></img>
         <p>【SNSで稼ぐ!!??】<br /><span>SNS型お小遣いアプリ🎵</span></p>
-        <a href="https://a-trade.jp/redirect/charin?media=G16531" class="btn-square">公式ページへ</a>
+        <a href="https://a-trade.jp/redirect/charin?media=G16531" class="btn-square" target="_blank">公式ページへ</a>
         <div class="line"></div>
         <!--ワクワク-->
         <img class="img-width" src="{{ asset('/images/wakuwaku.png') }}"></img>
         <p><span>【平均お手当時給 1万円超え!!??】</span><br />大手企業運営パパ活アプリ🎵</p>
-        <a href="https://fam-ad.com/ad/p/r?_site=64802&_article=16355" class="btn-square">紹介ページへ</a>
+        <a href="https://fam-ad.com/ad/p/r?_site=64802&_article=16355" class="btn-square" target="_blank">紹介ページへ</a>
         <div class="line"></div>
         <h3>まとめ</h3>
         <p>今回個人用に女性向け副業サービスをまとめて見ました。どれも評判良いサービスを中心にピックアップしており、特に<span>高時給と時間に都合付けれる点は良さそう🎵</span></p>
