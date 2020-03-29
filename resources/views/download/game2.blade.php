8c353b5c044640d7cdf326b1b97d8511abebd899
EC2 Default User <ec2-user@ip-172-31-36-190.us-east-2.compute.internal>
2020-03-29 19:58:43 +0000

    test

--- resources/views/download/game2.blade.php
+++ resources/views/download/game2.blade.php
@@ -12,6 +12,7 @@
         <br />
         <br />
         <p>ただいま公式キャンペーンページへ移動中です。<br><span>下記より対象OSを選択して下さい。</span></p>
+        <a href="https://a-trade.jp/redirect/nijikano?media=G16531"><img src="https://trading-ad.net/a-trade/banner/niji_600x500_021.jpg" alt=""></a>
         <a href="https://a-trade.jp/redirect/nijikano?media=G16531"><img src="{{ asset('/images/d_2.png') }}"></img></a>
         <p>移動後は、<span>「入手」ページより無料で</span>始められます。</p>
     </div>
