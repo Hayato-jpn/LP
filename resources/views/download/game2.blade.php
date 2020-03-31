99c09b2e1bcf06e985ec169950139471436ea5a4
EC2 Default User <ec2-user@ip-172-31-36-190.us-east-2.compute.internal>
2020-03-31 23:06:56 +0000

    gameLP

--- resources/views/download/game2.blade.php
+++ resources/views/download/game2.blade.php
@@ -3,17 +3,16 @@
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'OSを選択して下さい')
+@section('title', '公式ページへ遷移しています')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="body">
         <br />
-        <br />
-        <br />
-        <p>ただいま公式キャンペーンページへ移動中です。<br><span>下記より対象OSを選択して下さい。</span></p>
-        <!--<a href="https://a-trade.jp/redirect/nijikano?media=G16531" class="img-width"><img src="https://trading-ad.net/a-trade/banner/niji_600x500_021.jpg" alt=""></a>-->
-        <a href="https://a-trade.jp/redirect/nijikano?media=G16531"><img src="{{ asset('/images/d_2.png') }}"></img></a>
+        <p>ただいま公式キャンペーンページへ移動中です。<br />自動遷移しない場合、<span>再度コチラをタップして下さい。</span></p>
+        <a href="https://a-trade.jp/redirect/nijikano?media=G16531" class="btn-square">公式ページへ</a>
         <p>移動後は、<span>「入手」ページより無料で</span>始められます。</p>
+        <!--<a href="https://a-trade.jp/redirect/nijikano?media=G16531" class="img-width"><img src="https://trading-ad.net/a-trade/banner/niji_600x500_021.jpg" alt=""></a>-->
+        <a href="https://a-trade.jp/redirect/nijikano?media=G16531"><img src="{{ asset('/images/nijigen.png') }}" class="img-width"></img></a>
     </div>
 @endsection
