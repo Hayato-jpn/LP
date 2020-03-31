b1df7b01906060e4566d9b5719255079bc8941a3
EC2 Default User <ec2-user@ip-172-31-36-190.us-east-2.compute.internal>
2020-03-31 23:14:02 +0000

    second

--- resources/views/download/game2.blade.php
+++ resources/views/download/game2.blade.php
@@ -9,9 +9,9 @@
 @section('content')
     <div class="body">
         <br />
-        <p class="text-left">ただいま公式キャンペーンページへ移動中です。<br />自動遷移しない場合、<span>再度コチラをタップして下さい。</span></p>
+        <p class="text-left">ただいま公式キャンペーンページへ移動中です。<br />自動遷移しない場合、<span class="span-class">再度コチラをタップして下さい。</span></p>
         <a href="https://a-trade.jp/redirect/nijikano?media=G16531" class="btn-square">公式ページへ</a>
-        <p class="text-left">移動後は、<span>「入手」ページより無料で</span>始められます。</p>
+        <p class="text-left">移動後は、<span class="span-class">「入手」ページより無料で</span>始められます。</p>
         <!--<a href="https://a-trade.jp/redirect/nijikano?media=G16531" class="img-width"><img src="https://trading-ad.net/a-trade/banner/niji_600x500_021.jpg" alt=""></a>-->
         <a href="https://a-trade.jp/redirect/nijikano?media=G16531"><img src="{{ asset('/images/nijigen.png') }}" class="img-width"></img></a>
     </div>
