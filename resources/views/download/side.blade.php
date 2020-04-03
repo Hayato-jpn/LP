ef2f715450830af0c2c63797f0220b81244f41ba
EC2 Default User <ec2-user@ip-172-31-36-190.us-east-2.compute.internal>
2020-04-03 00:05:26 +0000

    edit

--- resources/views/download/side.blade.php
+++ resources/views/download/side.blade.php
@@ -10,7 +10,7 @@
     <header>
         <img src="{{ asset('/images/top2.jpg') }}" class="img-width-100"></img>
         <a name="top"></a>
-        <h3>▽ 2020年度「春」版！高時給 & 在宅バイト特集 ▽</h3>
+        <h3>【2020年度「春」版！】<br />高時給 & 在宅バイト特集！</h3>
         <img src="{{ asset('/images/person.jpg') }}" class="img-width editor"></img>
     </header>
     <div class="ranking">
@@ -18,45 +18,49 @@
         <div class="ikukuru">
             <div class="box14">
                 <p>登録人数1,300万人突破！</p>
-                <p>約20年の運営実績を誇る老舗アプリ</p>
-                <p>毎日2,000人を超えるユーザーが登録</p>
+                <p>約20年運営実績を誇る老舗アプリ</p>
+                <p>毎日2,000人以上が登録！</p>
                 <p>リッチな男性が多数在籍しているから、初心者でも簡単！</p>
             </div>
             <p>パパ活アプリでは無いものの良質な出会いがあることから人気のポイント♬<br /><span>高収入高学歴男性が多くパパ活活用している女性にも人気！</span></p>
             <a href="#" class="btn-square-so-pop">   公式アプリ   ▷︎</a>
+            <p>※公式アプリに飛びます↗️️</p>
         </div>
         <a href="https://a-trade.jp/redirect/biscor?media=G16531" target="_blank"><img src="{{ asset('/images/biscor_rank.jpg') }}" class="img-width"></img></a>
         <div class="biscor">
             <div class="box14">
                 <p>最高時給4,200円！</p>
-                <p>ライブ配信でインフルエンサーデビュー!?</p>
+                <p>ライブ配信でデビュー!?</p>
                 <p>勿論、顔出しNGもOK！</p>
                 <p>スマホでスキマ時間から働けます</p>
             </div>
             <p>おしゃべり好き必見！<br /><span>会員ユーザーとお話・悩み相談を受けるだけのバイト♬</span><br />スマホでスキマ時間に始められるから、忙しい方にも！</p>
             <a href="https://a-trade.jp/redirect/biscor?media=G16531" class="btn-square-so-pop" target="_blank">   公式ページ   ▷︎</a>
+            <p>※公式ページに飛びます↗️️</p>
         </div>
         <a href="https://a-trade.jp/redirect/moa?media=G16531" target="_blank"><img src="{{ asset('/images/rank_moa.jpg') }}" class="img-width"></img></a>
         <div class="moa">
             <div class="box14">
                 <p>最高時給3,600円！</p>
-                <p>LINE感覚でチャット・通話するだけ♬</p>
+                <p>LINE感覚でお話するだけ♬</p>
                 <p>だから、好きな時間・場所でOK！</p>
                 <p>今、巷で女性ユーザー急増中</p>
             </div>
-            <p>ユーザー間のやり取りは専用アプリを使うので<br /><span>身バレの心配ありません！</span></p>
+            <p>ユーザー間のやり取りは専用アプリを使うので<span>身バレの心配ありません！</span></p>
             <a href="https://a-trade.jp/redirect/moa?media=G16531" class="btn-square-so-pop" target="_blank">   公式ページ   ▷︎</a>
+            <p>※公式ページに飛びます↗️️</p>
         </div>
         <a href="https://a-trade.jp/redirect/charin?media=G16531" target="_blank"><img src="{{ asset('/images/rank_charin.jpg') }}" class="img-width"></img></a>
         <div class="charin">
             <div class="box14">
                 <p>最高時給4,200円！</p>
                 <p>SNS感覚で利用可能♬</p>
-                <p>Twitter, instagram利用者にはピッタリ！</p>
+                <p>SNS利用者にはピッタリ！</p>
                 <p>新規サービスでユーザー募集中</p>
             </div>
-            <p>専用アプリだけで<span>月10万円達成者「多数！」</span><br />手堅く稼ぎたいならコレ！</p>
+            <p>専用アプリだけで<span>月10万円達成者「多数！」</span>手堅く稼ぎたいならコレ！</p>
             <a href="https://a-trade.jp/redirect/charin?media=G16531" class="btn-square-so-pop" target="_blank">   公式ページ   ▷︎</a>
+            <p>※公式ページに飛びます↗️️</p>
         </div>
     </div>
     <div class="line"></div>
