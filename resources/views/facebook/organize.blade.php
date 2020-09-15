b475d8fce3ab32cc2b47101159f5bc0977e0d7dc
EC2 Default User <ec2-user@ip-172-31-36-190.us-east-2.compute.internal>
2020-09-15 21:08:36 +0000

    mm

--- resources/views/facebook/organize.blade.php
+++ resources/views/facebook/organize.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>テスト</title>
+        <title>EBURA.</title>
         <link href="{{ secure_asset('css/organize.css') }}" rel="stylesheet">
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
